b58aa5b0070354258bb911a3d0db2d6ffd3d3e21
Paul Thomas <paulthomas2107@gmail.com>
2023-02-08 15:08:04 +0000

    Initial

---
+++ game.py
@@ -0,0 +1,34 @@
+import random
+
+COLORS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 10
+CODE_LENGTH = 4
+
+
+def generate_code():
+    code = []
+
+    for _ in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+        code.append(color)
+
+    return code
+
+
+def guess_code():
+
+    while True:
+        guess = input("Guess: ").upper().split(" ")
+
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} colors.")
+            continue
+
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color: {color}. Try again.")
+                break
+        else:
+            break
+
+    return guess
